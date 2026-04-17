81719dce523b392c6d3756815393be3d0618aeb8
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-04-17 14:44:51 +0200

    Initial Commit

---
+++ fridayPractice/fridayPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fridayPracticeApp.swift
+//  fridayPractice
+//
+//  Created by neda khalajnejad on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct fridayPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
